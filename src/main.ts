e0fb2d2f088bfd0b75586a11ec866a910b86186b
0fk0 <greeeen_ken41ei39jin@icloud.com>
2024-06-29 10:39:45 +0900

    fix: シーン名の変更

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
+import { Result } from './scenes/Result';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
@@ -24,7 +24,7 @@ const config: Types.Core.GameConfig = {
         Preloader,
         MainMenu,
         MainGame,
-        GameOver
+        Result
     ],
     dom: {
 		createContainer: true

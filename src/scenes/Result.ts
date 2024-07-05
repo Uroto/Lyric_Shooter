6eff79b731009bbc01a83b8308102ddf57f3b27f
0fk0 <greeeen_ken41ei39jin@icloud.com>
2024-07-05 10:46:32 +0900

    fix: 終了シーンのテキスト位置・フォント変更とホバー機能の追加

--- src/scenes/Result.ts
+++ src/scenes/Result.ts
@@ -5,7 +5,7 @@ export class Result extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera | undefined;
     background: Phaser.GameObjects.Graphics | undefined;
-    gameover_text : Phaser.GameObjects.Text | undefined;
+    backToMainMenu_text : Phaser.GameObjects.Text | undefined;
     bonus_text: Phaser.GameObjects.Text | undefined;
     score: number | undefined;
     player: Player | undefined;
@@ -16,67 +16,45 @@ export class Result extends Scene
     }
 
     create ()
-    {
+    {   
+        const bgWidth = window.innerWidth / 2;
+        const bgHeight = window.innerHeight / 2;
+        const bgX = window.innerWidth / 4;
+        const bgY = window.innerHeight / 4;
+
         this.background = this.add.graphics()
                                   .fillStyle(0x000000, 1)
-                                  .fillRoundedRect(window.innerWidth / 4, window.innerHeight / 4, window.innerWidth / 2, window.innerHeight / 2, 20)
+                                  .fillRoundedRect(bgX, bgY, bgWidth, bgHeight, 20)
                                   .setAlpha(0.8);
 
         this.player = this.registry.get('player');
         this.score = this.registry.get('score');
 
-        this.add.text(window.innerWidth / 2, window.innerHeight / 2, 'Score: ' + this.score?.toString(), {
-            fontFamily: 'Arial', fontSize: 60, color: '#ffffff',
+        this.add.text(bgX + bgWidth / 2, bgY + bgHeight / 4, 'FINISH!!', {
+            fontFamily: 'mihiPixelmoji', fontSize: 100, color: '#ffffff',
             align: 'center'
         }).setOrigin(0.5);
 
-        this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 100, 'Click to continue', {
-            fontFamily: 'Arial', fontSize: 30, color: '#ffffff',
+        this.add.text(bgX + bgWidth / 2, bgY + bgHeight / 2, 'Score: ' + this.score?.toString(), {
+            fontFamily: 'mihiPixelmoji', fontSize: 50, color: '#ffffff',
             align: 'center'
-        }).setOrigin(0.5)
-          .setInteractive()
-          .on('pointerdown', () => {
-            this.scene.stop('Stage1');
-            this.scene.stop('Stage2');
-            this.scene.stop('Stage3');
-            this.scene.stop('Result');
-
-            if (this.player?.video) {
-                this.player.requestStop();
-                this.player.requestPlay();
-            }
-
-            const previousScene = this.registry.get('previousScene');
-            this.scene.start(previousScene);
-            this.registry.set('score', 0);
-            this.registry.set('previousScene', 'Result');
-        });
-
-        this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 200, 'Back to Main Menu', {
-            fontFamily: 'Arial', fontSize: 30, color: '#ffffff',
-            align: 'center'
-        }).setOrigin(0.5)
-          .setInteractive()
-          .on('pointerdown', () => {
-            this.scene.stop('Stage1');
-            this.scene.stop('Stage2');
-            this.scene.stop('Stage3');
-            this.scene.stop('Result');
-            this.scene.start('MainMenu');
-        });
+        }).setOrigin(0.5);
 
         const previousScene = this.registry.get('previousScene');
         const max_score = this.registry.get('max_score');
-        if (this.score && previousScene !== 'Stage3' && this.score >= max_score * 0.9) {
-            this.bonus_text = this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 300, 'Bonus Stage!!', {
-                fontFamily: 'Arial', fontSize: 60, color: '#ffffff',
+        if (this.score !== undefined && previousScene !== 'Stage3' && this.score >= 0) {
+            this.bonus_text = this.add.text(bgX + 3 * bgWidth / 4, bgY + 3 * bgHeight / 4, 'Bonus Stage!!', {
+                fontFamily: 'mihiPixelmoji', fontSize: 30, color: '#ffffff',
                 align: 'center'
                 
             }).setOrigin(0.5)
               .setActive(false)
               .setAlpha(0.5)
-              .setInteractive()
-              .on('pointerdown', () => {
+              .setInteractive();
+
+            const underline = this.underline(this.bonus_text);
+            
+            this.bonus_text?.on('pointerdown', () => {
                     this.player?.requestStop(); // まず一時停止
                     this.player?.requestPlay();  // その後再生
 
@@ -85,8 +63,41 @@ export class Result extends Scene
                     this.scene.stop('Stage3');
                     this.scene.stop('Result');
                     this.scene.start('Stage3');
+            })
+              .on('pointerover', function (this: Phaser.GameObjects.Text) {
+                underline.setVisible(true);
+            })
+              .on('pointerout', function (this: Phaser.GameObjects.Text) {
+                underline.setVisible(false);
             });
         }
+
+        let backToMainMenueX;
+        if (this.bonus_text && previousScene !== 'Stage3'){
+            backToMainMenueX = bgX + bgWidth / 4;
+        } else {
+            backToMainMenueX = bgX + bgWidth / 2;
+        }
+        this.backToMainMenu_text = this.add.text(backToMainMenueX, bgY + 3 * bgHeight / 4, 'Back to Main Menu', {
+            fontFamily: 'mihiPixelmoji', fontSize: 30, color: '#ffffff',
+        }).setOrigin(0.5)
+          .setInteractive();
+        
+        const underline = this.underline(this.backToMainMenu_text);
+
+        this.backToMainMenu_text?.on('pointerdown', () => {
+            this.scene.stop('Stage1');
+            this.scene.stop('Stage2');
+            this.scene.stop('Stage3');
+            this.scene.stop('Result');
+            this.scene.start('MainMenu');
+        })
+          .on('pointerover', function (this: Phaser.GameObjects.Text) {
+            underline.setVisible(true);
+        })
+          .on('pointerout', function (this: Phaser.GameObjects.Text) {
+            underline.setVisible(false);
+        });
 
         const songUrl = "https://piapro.jp/t/ELIC/20240130010349";
         this.player?.createFromSongUrl(songUrl)
@@ -94,5 +105,16 @@ export class Result extends Scene
                         this.bonus_text?.setActive(true);
                         this.bonus_text?.setAlpha(1);
                     });
+    }
+
+    underline(text: Phaser.GameObjects.Text): Phaser.GameObjects.Graphics {
+        const underline = this.add.graphics();
+        underline.lineStyle(3, 0xffffff, 1);
+        underline.moveTo(text.x - text.width / 2, text.y + text.height / 2 + 5);
+        underline.lineTo(text.x + text.width / 2, text.y + text.height / 2 + 5);
+        underline.strokePath();
+        underline.setVisible(false);
+
+        return underline;
     }
 }

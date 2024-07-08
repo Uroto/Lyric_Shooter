63f59e38f6bb4ba2c37d22bb1c7b4849a2c64e7b
0fk0 <greeeen_ken41ei39jin@icloud.com>
2024-07-08 18:54:51 +0900

    fix: ボーナスステージ後の再度stage1, 2へのプレイでResult画面がボーナスあり状態のままになるバグを修正

--- src/scenes/Result.ts
+++ src/scenes/Result.ts
@@ -44,6 +44,7 @@ export class Result extends Scene
         }).setOrigin(0.5);
 
         const max_score = this.registry.get('max_score');
+        this.bonus_text = undefined;
         if (this.score !== undefined && previousScene !== 'Stage3' && this.score >= max_score * 0.9) {
             this.bonus_text = this.add.text(bgX + 3 * bgWidth / 4, bgY + 3 * bgHeight / 4, 'Bonus Stage!!', {
                 fontFamily: 'mihiPixelmoji', fontSize: 30, color: '#ffffff',

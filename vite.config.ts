f5b7167786baadd23e05aca7c05c6a5501943d70
0fk0 <greeeen_ken41ei39jin@icloud.com>
2024-07-09 09:35:27 +0900

    fix: アセット読み込みの階層を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/',
+    base: '/Lyric_Shooter/',
     build: {
         assetsInlineLimit: 0,
         outDir: 'dist',
